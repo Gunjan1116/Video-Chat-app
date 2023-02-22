ea8b0b00dd9db672c81af9847aba6c97d2f7b174
ankit711 <itsankitsingh711@edureka.co>
2023-02-22 13:47:00 +0530

    build emit to message

--- Frontend/Scripts/index.js
+++ Frontend/Scripts/index.js
@@ -1,5 +1,16 @@
-const socket=io("http://localhost:5000/",{transports:["websocket"]})
+const socket=io("http://localhost:5000/",{transports:["websocket"]});
+
+const input_msg = document.querySelector("form");
+
+input_msg.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = document.querySelector("#message").value;
+    const p = document.createElement("p");
+    p.innerText = message;
+})
+
 
 socket.on("welcome",(msg)=>{
     console.log(msg);
-})
+});
+
