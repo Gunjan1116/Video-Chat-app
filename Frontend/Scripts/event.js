e2ead84b8e2ba5b8cf3b23873052da66af1fe65a
Tushargandhi07 <tushargan7@gmail.com>
2023-02-23 20:38:16 +0530

    socket connect to frontend

---
+++ Frontend/Scripts/event.js
@@ -0,0 +1,21 @@
+import * as state from "./state.js"
+import * as change from "./change.js"
+import * as WebRtc from "./WebRtc.js"
+let socketIo;
+export const registerSocketEvent=(socket)=>{
+    socket.on("connect",()=>{
+        socketIo=socket
+        console.log("successfully connected to wss server");
+        //console.log(socket.id)
+        state.setSocketId(socket.id)
+        change.updatePersonalCode(socket.id)
+    })
+    socket.on("preOffers",(data)=>{
+        console.log("getting pre offers");
+        WebRtc.RecivingPreOffer(data)
+    })
+}
+export const preOffers=(data)=>{
+    console.log("sending pre offers")
+    socketIo.emit("preOffer",data)
+}
