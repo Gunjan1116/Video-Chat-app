98e99848bf9962eafaa86f69b138e7dfdc65e6bb
ankit711 <itsankitsingh711@edureka.co>
2023-02-25 21:02:30 +0530

    local video calling functionality

--- Frontend/Scripts/WebRtc.js
+++ Frontend/Scripts/WebRtc.js
@@ -4,6 +4,56 @@ import * as change from "./change.js";
 // <---------- Sending offer to the other user ------------>
 
 let connectedUserDetails;
+const defaultConstraints={
+    audio:true,
+    video:true
+}
+const configuration={
+    iceServers:[
+        {
+            urls: "stun:stun.1.google.com:13902"
+        }
+    ]
+}
+export const getLocalPreview=()=>{
+    navigator.mediaDevices.getUserMedia(defaultConstraints)
+    .then((stream)=>{
+       change.updateLocalVideo(stream);
+        state.setLocalStream(stream);
+    }).catch((err)=>{
+        console.log("error occured when trying to access camera",err)
+    })
+}
+const createPeerConnection=()=>{
+    peerConnection= new RTCPeerConnection(configuration)
+    peerConnection.onicecandidate=(event)=>{
+        console.log("geeting ice candidates from stun server")
+        if(event.candidate){
+            //sending our ice candiadate to other peer
+        }
+    }
+    peerConnection.onconnectionstatechange= (event)=>{
+        if(peerConnection.connectionState==="connected"){
+            console.log("successfully connected to other peer")
+        }
+    }
+    //receiving track
+    const remoteStream=new MediaStream();
+    state.setRemoteStream(remoteStream);
+    change.updateRemoteVideo(remoteStream);
+    peerConnection.ontrack=(event)=>{
+        remoteStream.addTrack(event.track);
+    }
+    //add our stream to peer connection
+    if(connectedUserDetails.connection_type==="personal_code_video"){
+        const localStream= state.getState().localStream;
+        for(const track of localStream.getTrack()){
+            peerConnection.addTrack(track,localStream)
+        }
+    }
+}
+
+
 export const preOffers=(connection_type,personal_code)=>{
     console.log(connection_type,personal_code)
     connectedUserDetails={
